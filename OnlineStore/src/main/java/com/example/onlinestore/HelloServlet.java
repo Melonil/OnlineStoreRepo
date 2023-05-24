c562c588a2d881ea67f6988b77b0d5f0a5b18332
Melonil <yaci.rachidi@gmail.com>
2023-05-24 16:20:07 +0200

    nouveau test sur intellij ave cgit

--- OnlineStore/src/main/java/com/example/onlinestore/HelloServlet.java
+++ OnlineStore/src/main/java/com/example/onlinestore/HelloServlet.java
@@ -19,7 +19,7 @@ public class HelloServlet extends HttpServlet {
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>ok" + message + "</h1>");
+        out.println("<h1>ok " + message + "</h1>");
         out.println("</body></html>");
     }
 
